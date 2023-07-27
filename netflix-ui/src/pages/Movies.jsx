55d34db961119ce6ffe3f2a5875c476323569e88
diwakar1593 <diwakar200kumar@gmail.com>
2023-07-27 20:06:33 +0530

    commit

--- netflix-ui/src/pages/Movies.jsx
+++ netflix-ui/src/pages/Movies.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Navbar from "../components/Navbar";
+import {
+  createAsyncThunk
+} from "@reduxjs/toolkit";
 import { onAuthStateChanged } from "firebase/auth";
 import { firebaseAuth } from "../utils/firebase-config";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { fetchMovies, getGenres } from "../store";
+import { API_KEY, TMDB_BASE_URL } from "../utils/constants";
 import SelectGenre from "../components/SelectGenre";
+import axios from "axios";
 import Slider from "../components/Slider";
 import NotAvailable from "../components/NotAvailable";
 
@@ -20,10 +24,57 @@ function MoviePage() {
   const dispatch = useDispatch();
 
   useEffect(() => {
+    const getGenres = createAsyncThunk("netflix/genres", async () => {
+      const {
+        data: { genres },
+      } = await axios.get(
+        "https://api.themoviedb.org/3/genre/movie/list?api_key=174b9b0ac802be24ade7f2956faba9ba"
+      );
+      return genres;
+    });
     dispatch(getGenres());
   }, []);
 
   useEffect(() => {
+    const createArrayFromRawData = (array, moviesArray, genres) => {
+      array.forEach((movie) => {
+        const movieGenres = [];
+        movie.genre_ids.forEach((genre) => {
+          const name = genres.find(({ id }) => id === genre);
+          if (name) movieGenres.push(name.name);
+        });
+        if (movie.backdrop_path)
+          moviesArray.push({
+            id: movie.id,
+            name: movie?.original_name ? movie.original_name : movie.original_title,
+            image: movie.backdrop_path,
+            genres: movieGenres.slice(0, 3),
+          });
+      });
+    };
+    const getRawData = async (api, genres, paging = false) => {
+      const moviesArray = [];
+      for (let i = 1; moviesArray.length < 60 && i < 10; i++) {
+        const {
+          data: { results },
+        } = await axios.get(`${api}${paging ? `&page=${i}` : ""}`);
+        createArrayFromRawData(results, moviesArray, genres);
+      }
+      return moviesArray;
+    };
+    const fetchMovies = createAsyncThunk(
+      "netflix/trending",
+      async ({ type }, thunkAPI) => {
+        const {
+          netflix: { genres },
+        } = thunkAPI.getState();
+        return getRawData(
+          `${TMDB_BASE_URL}/trending/${type}/week?api_key=${API_KEY}`,
+          genres,
+          true
+        );
+      }
+    );
     if (genresLoaded) {
       dispatch(fetchMovies({ genres, type: "movie" }));
     }
